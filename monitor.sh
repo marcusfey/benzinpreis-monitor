15defe11075e7895d61b289e925ffb4d7a80c676
Marcus Fey <mfey@gmx.de>
2022-01-09 17:01:17 +0100

    Better performance for multiple station_ids (just 1 query)

--- monitor.sh
+++ monitor.sh
@@ -4,22 +4,36 @@ APIKEY=$(cat apikey)
 TELEGRAMBOT=$(cat telegrambot)
 TELEGRAMCHATID=$(cat telegramchatid)
 
+set +x
+
+ids=$(cat station_ids  | tr '\n' , | sed s/,*$// )
+#echo $ids
+raw=$(curl -s  "https://creativecommons.tankerkoenig.de/json/prices.php?ids=$ids&apikey=$APIKEY" ) 
+#echo $raw > raw.log
+#raw=$(cat raw.log)
+prices=$(echo $raw | jq " . |.prices")
+
+echo $prices > prices.log
+
 for id in $(cat station_ids); 
 do 
-    #curl -s  "https://creativecommons.tankerkoenig.de/json/prices.php?ids=$id&apikey=$APIKEY" | jq ". | .prices|map(.)|.[0]|.\"e5\",.diesel" > stations/$id.curr
-    curl -s  "https://creativecommons.tankerkoenig.de/json/prices.php?ids=$id&apikey=$APIKEY" | jq ". | .prices|map(.)|.[0]" > stations/$id.curr
+    echo $prices | jq ".\"$id\"" | jq -r "( .status,.\"e5\",.\"e10\", .\"diesel\")" | tr '\n' '\t'  | sed -e s/\t$// > stations/$id.curr
+
     cmp -s stations/$id.prev stations/$id.curr
     if [[ $? == 0 ]]; then
         echo "same at $id"
     else
         echo "diff at $id: "
-
-        name=$(curl -s "https://creativecommons.tankerkoenig.de/json/detail.php?id=$id&apikey=$APIKEY" | jq ".station.name")
+        res=$(curl -s "https://creativecommons.tankerkoenig.de/json/detail.php?id=$id&apikey=$APIKEY" )
+        echo $res
+        name=$(echo $res| jq  -r "(.station.name +\" \"+ .station.street)")
+        echo "name:$name"
 
         text="Benzinpreis-Update:
 $name
-$(cat stations/$id.curr)"
-        echo $text
+alt:$(cat stations/$id.prev)
+neu:$(cat stations/$id.curr)"
+        echo "text: $text"
         curl --silent \
                   --data-urlencode "chat_id=$TELEGRAMCHATID" \
                     --data-urlencode "text=$text" \
